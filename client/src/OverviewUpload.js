2ef79b6d8754bb57a79ed90510a2c44cf5bacf00
Mike Drum <mdrum@psi.edu>
2022-09-08 10:17:14 -0700

    wait for image to load before drawing and setting dimensions

--- client/src/OverviewUpload.js
+++ client/src/OverviewUpload.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import ImageUploader from "./components/ImageUploader";
 import Canvas from './components/Canvas'
 
@@ -27,22 +27,36 @@ function Uploader({callback}) {
 }
 
 function Overview({imageUrl}) {
+  const [drawContext, setDrawContext] = useState(null)
+  const [loaded, setLoaded] = useState(false)
   const img = useRef(null) 
 
-  const plotImage = (ctx) => {
-    ctx.drawImage(img.current, 0, 0)
-  }
-
+  
   const clickHandler = (x, y) => {
     console.log(`${x}, ${y}`)
   }
-
+  
   const width = img.current?.naturalWidth || 1000
   const height = img.current?.naturalHeight || 1000
+  
+  if(loaded) {
+    drawContext.drawImage(img.current, 0, 0)
+  }
+
+  useEffect(() => {
+    const listener = () => {
+      setLoaded(true)
+    }
+    img.current.addEventListener('load', listener)
+
+    return function cleanup() {
+      img.current.removeEventListener('load', listener)
+    }
+  }, [])
 
   return <>
     <img ref={img} src={imageUrl} style={{display: 'none'}} crossOrigin="anonymous"/>
-    <Canvas context={plotImage} click={clickHandler} width={width} height={height}/>
+    <Canvas context={setDrawContext} click={clickHandler} width={width} height={height}/>
   </>
 
 }
