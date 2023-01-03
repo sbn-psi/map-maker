b3bf8de46fd34835c897b24ccc6976d70925a634
Mike Drum <mdrum@psi.edu>
2023-01-03 16:39:05 -0700

    add natural image dimensions to output

--- client/src/AppState.ts
+++ client/src/AppState.ts
@@ -18,12 +18,24 @@ export class Zone {
     left?: { x: number, y: number }
     bottom?: { x: number, y: number }
     height?: number
+    naturalHeight?: number
+    naturalWidth?: number
     isComplete: () => boolean = () => {
         return !!this.top && !!this.left && !!(this.bottom || this.height)
     }
     constructor(name: string, url: string) {
         this.name = name
         this.url = url
+    }
+    static fromJson(json: any): Zone {
+        let zone = new Zone(json.name, json.url)
+        zone.top = json.top
+        zone.left = json.left
+        zone.bottom = json.bottom
+        zone.height = json.height
+        zone.naturalHeight = json.naturalHeight
+        zone.naturalWidth = json.naturalWidth
+        return zone
     }
 }
 
--- client/src/components/ImageUploader.tsx
+++ client/src/components/ImageUploader.tsx
@@ -16,6 +16,7 @@ export default function ImageUploader({handler, cardinality = "single"}: Props){
 		setSelectedFiles(target.files)
 	};
 
+	let fileDimensions:{[key: string]: {width: number, height: number}} = {}
 	const handleSubmission = () => {
 		if(!selectedFiles) return
 
@@ -23,10 +24,13 @@ export default function ImageUploader({handler, cardinality = "single"}: Props){
 
 		for(let file of Array.from(selectedFiles)) {
 			formData.append(file.name, file)
+			getUploadedFileDimensions(file).then(dimensions => {
+				fileDimensions[file.name] = dimensions
+			})
 		}
 
 		web.post('/image/upload', formData).then(resp => {
-			handler(resp.data.map((zone: any) => new Zone(zone.name, zone.url)))
+			handler(resp.data.map((zone: any) => Zone.fromJson({...zone, naturalWidth: fileDimensions[zone.name].width, naturalHeight: fileDimensions[zone.name].height})))
 			setSelectedFiles(null)
 		}, err => {
 			console.error(err)
@@ -55,3 +59,24 @@ export default function ImageUploader({handler, cardinality = "single"}: Props){
 		</div>
 	)
 }
+
+function getUploadedFileDimensions(file: File) {
+	return new Promise<{width: number, height: number}>((resolve, reject) => {
+		try {
+			let img = new Image()
+
+			img.onload = () => {
+				const width  = img.naturalWidth,
+					height = img.naturalHeight
+
+				window.URL.revokeObjectURL(img.src)
+
+				return resolve({width, height})
+			}
+
+			img.src = window.URL.createObjectURL(file)
+		} catch (exception) {
+			return reject(exception)
+		}
+	}
+)}
